710f7e72ca724d1ad2029cbfc06f9ab5dad1de50
Landonwilson1 <Landonjwilson2@gmail.com>
2021-05-20 15:57:27 -0400

    updated markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -8,7 +8,6 @@ function renderLicenseBadge(license) {
           return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
           break;
       case "MIT":
-          // code block
           return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
           break;
       default: return "";
